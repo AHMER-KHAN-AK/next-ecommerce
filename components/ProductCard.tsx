6e023a61829e8b12cea30569626757b36bdd549d
AHMER-KHAN-AK <m.ahmerkhan100@gmail.com>
2024-12-08 12:20:56 -0800

    initial commit

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,20 +6,15 @@ const ProductCard = ({
 }: {
   product: {
     title: string;
-    category: string;
     price: number;
     thumbnail: string;
   };
 }) => {
-  const { title, category, price, thumbnail } = product;
+  const { title, price, thumbnail } = product;
   return (
     <div className="lg:w-1/5 md:w-1/2 p-4 w-full ">
       <a className="block relative h-48 rounded overflow-hidden">
-        {/* <img
-          alt="ecommerce"
-          className="object-cover object-center w-full h-full block"
-          src="https://dummyimage.com/420x260"
-        /> */}
+        
         <Image src={thumbnail} alt={title} width={420} height={260} />
       </a>
       <div className="mt-4">
